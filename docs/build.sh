3fedc4005119988890695bd1da89c5d67947ecf0
wmdaniel <wmdaniel@noreply.github.com>
2023-06-28 08:01:02 -0400

    Rebuild: Wed Jun 28 08:01:02 AM EDT 2023

---
+++ docs/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Remove existing docs folder 
+if [ -d docs ]; then
+    rm -rf docs
+fi
+
+# Rebuild the site
+npx honkit build
+
+# Copy the `_book` folder contents to `docs`
+cp -Rf _book docs
+
+# Create a new commit for the rebuild (every time), maybe with a date?
+git add docs/
+git commit -m "Rebuild: $(date)"
+
+# Push the commit to GitHub.
+git push
